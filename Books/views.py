2b907c43c4b64b36c4ee2d7295e079c4b910c561
Korbin Dansie <dansiekm@gmail.com>
2023-10-28 15:55:54 -0600

    Set up pagination

--- Books/views.py
+++ Books/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from django.core.paginator import Paginator
 
 from .models import BookData
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
     book_list = BookData.objects.all()
+    paginator = Paginator(book_list, 5)  # Show 5 contacts per page.
+    
+    page_number = request.GET.get("page")
+    
+    book_obj = paginator.get_page(page_number)
+    page_range = paginator.get_elided_page_range(number=page_number) # add ellipsis
 
     my_context = {
-        "book_list": book_list,
+        "book_list": book_obj,
+        "page_range": page_range,
         "site_title": "Home"
     }
     return render(request, "home.html", my_context) # return an html template
