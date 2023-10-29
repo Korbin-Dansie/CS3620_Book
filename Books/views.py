01d0a25fade1e33083e0322f54146209d9796324
Korbin Dansie <dansiekm@gmail.com>
2023-10-29 15:55:07 -0600

    Keep params per page

--- Books/views.py
+++ Books/views.py
@@ -5,18 +5,25 @@ from .models import BookData
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    book_list = BookData.objects.all()
-    paginator = Paginator(book_list, 5)  # Show 5 contacts per page.
+    book_obj = BookData.objects.all() # also a queryset qs
+
+    # Filter dynamicly with a form 
+    # Filter based on variables for a book name
+    book_name = request.GET.get("book_name")
+    if book_name != "" and book_name is not None:
+        book_obj = book_obj.filter(name__icontains = book_name)
+
+    paginator = Paginator(book_obj, 5)  # Show 5 contacts per page.
     
     page_number = request.GET.get("page")
     if page_number == "" or page_number == None:
         page_number = 1
     
-    book_obj = paginator.get_page(page_number)
+    book_paginator = paginator.get_page(page_number)
     page_range = paginator.get_elided_page_range(number=page_number) # add ellipsis
 
     my_context = {
-        "book_list": book_obj,
+        "book_list": book_paginator,
         "page_range": page_range,
         "site_title": "Home"
     }
